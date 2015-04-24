23797dac81562f0e6f1c706f6574fcc2695a21cf
Christian Arevalo <contact@christianarevalo.ch>
2015-04-24 15:04:13 +0200

    Add Product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424123415) do
+ActiveRecord::Schema.define(version: 20150424130214) do
 
   create_table "buyers", force: true do |t|
     t.string   "name",                                 null: false
@@ -32,5 +32,12 @@ ActiveRecord::Schema.define(version: 20150424123415) do
 
   add_index "buyers", ["email"], name: "index_buyers_on_email", unique: true
   add_index "buyers", ["reset_password_token"], name: "index_buyers_on_reset_password_token", unique: true
+
+  create_table "products", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
